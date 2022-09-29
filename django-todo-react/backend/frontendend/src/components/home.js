c1f2e2d20a061be2756456a888a717585ae6abde
pescado <aisuruyamano@gmail.com>
2022-09-29 17:53:58 +0900

    Added files and changes required for #

--- django-todo-react/backend/frontendend/src/components/home.js
+++ django-todo-react/backend/frontendend/src/components/home.js
@@ -5,11 +5,7 @@ import Signup from "./signup";
 const Home = (props) =>{
 
 
-  const handleLogin = (data) =>{
-     props.setLoggedInStatus("ログインなう")
-     props.setUser(data.user)
-     console.log("miss")
-  }
+
 
 
 
@@ -27,7 +23,6 @@ const handleSuccessfulAuthentication = (data) =>{
   <div>
     <h1>Home</h1>
     <h2>ログイン状態:{props.loggedInStatus}</h2>
-    <Signup handleSuccessfulAuthentication={handleSuccessfulAuthentication} handleLogin={handleLogin}/>
   </div>
   )
 }
