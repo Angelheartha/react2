b9bebaf1b386a39014ddf3b051787648ee21a541
pescado <aisuruyamano@gmail.com>
2022-11-28 11:45:11 +0900

    @vbjbhv@

---
+++ django-todo-react/frontend/pages/_app.tsx
@@ -0,0 +1,16 @@
+import '../styles/globals.css'
+import Navbar from "../src/components/navbar";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <div>
+      <Navbar />
+      <div className="golinks">
+        <h1 className="message">You are always welcome!</h1>
+        <Component {...pageProps} />
+      </div>
+    </div>
+  )
+}
+
+export default MyApp
